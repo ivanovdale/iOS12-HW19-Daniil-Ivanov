13b71705999058b13fa592b80eea627942b96651
ivanovdale <dania-spb@mail.ru>
2024-03-20 12:09:17 +0300

    Initial commit

---
+++ Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func getData(urlRequest: String) {
+    let urlRequest = URL(string: urlRequest)
+    guard let url = urlRequest else { return }
+    URLSession.shared.dataTask(with: url) { data, response, error in
+        if error != nil {
+
+        } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+            guard let data = data else { return }
+            let dataAsString = String(data: data, encoding: .utf8)
+            
+        }
+    }.resume()
+}
