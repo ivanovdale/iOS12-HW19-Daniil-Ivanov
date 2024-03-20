8b2dd94edab24ffb9209816fe495cdccab7c57a0
ivanovdale <dania-spb@mail.ru>
2024-03-20 17:00:50 +0300

    refactor marvel api to use URLComponents

--- Sources/Api/MarvelApi.swift
+++ Sources/Api/MarvelApi.swift
@@ -2,6 +2,7 @@ import Foundation
 
 public final class MarvelApi {
     private enum Constants {
+        static let scheme = "https"
         static let host = "gateway.marvel.com"
         static let publicApiKey = "4c43dd559df35e4102ed9be981186419"
         static let privateApiKey = "34910bd2da65fd23a364ce6cdcf370072560a28e"
@@ -13,24 +14,31 @@ public final class MarvelApi {
         self.client = client
     }
 
-    private static var hashParam: String {
-        let timestamp = Date().currentTimeInMillis()
-        let tsParam = "ts=\(timestamp)"
-        let hashValue = (String(timestamp) + Constants.privateApiKey + Constants.publicApiKey).md5Hash()
-        let hashParam = "&hash=\(hashValue)"
-        return tsParam + hashParam
-    }
-
     private enum Endpoint {
         case characterComics(id: String)
 
         var urlRequest: String {
             switch self {
             case .characterComics(let id):
-                let apiKeyParam = "apikey=\(Constants.publicApiKey)"
-                let queryParameters = "?\(hashParam)&\(apiKeyParam)"
-                return "https://" + Constants.host + "/v1/public/characters/\(id)/comics" + queryParameters
+                var components = URLComponents()
+                components.scheme = Constants.scheme
+                components.host = Constants.host
+                components.path = "/v1/public/characters/\(id)/comics"
+                components.queryItems = buildQueryItems()
+
+                return components.url?.absoluteString ?? ""
             }
+        }
+
+        private func buildQueryItems() -> [URLQueryItem] {
+            let timestamp = String(Date().currentTimeInMillis())
+            let hashValue = (timestamp + Constants.privateApiKey + Constants.publicApiKey).md5Hash()
+
+            return [
+                URLQueryItem(name: "apikey", value: Constants.publicApiKey),
+                URLQueryItem(name: "ts", value: timestamp),
+                URLQueryItem(name: "hash", value: hashValue)
+            ]
         }
     }
 
