b81e7e6d7b92cc9b2b0cfcc5c3232fd2310593a0
ivanovdale <dania-spb@mail.ru>
2024-03-20 16:19:00 +0300

    refactor use case to using cbr api and http client

--- Contents.swift
+++ Contents.swift
@@ -1,55 +1,16 @@
 import Foundation
 
-// MARK: - Constants
-
-enum Constants {
-    static let cbrHost = "www.cbr.ru"
-}
-
-// MARK: - Data getting
-
-func getData(urlRequest: String) {
-    let urlRequest = URL(string: urlRequest)
-    guard let url = urlRequest else { return }
-    URLSession.shared.dataTask(with: url) { data, response, error in
-        if let error = error as? URLError {
-            print("Error occured.")
-            switch error.code {
-            case .notConnectedToInternet:
-                print("The Internet connection appears to be offline. Please reconnect")
-            case .cannotFindHost:
-                let host = url.host ?? "Unknown"
-                print("Cannot find host \(host)")
-            default: print("An error occurred: \(error.localizedDescription)")
-            }
-        } else if let response = response as? HTTPURLResponse {
-            print("Response code: \(response.statusCode)")
-            if response.statusCode == 200 {
-                guard let data = data else { return }
-                let encoding: String.Encoding
-                if url.host == Constants.cbrHost{
-                    encoding = .windowsCP1251
-                } else {
-                    encoding = .utf8
-                }
-                let dataAsString = String(data: data, encoding: encoding)
-                guard let dataAsString = dataAsString else { return }
-                print(dataAsString)
-            } else if response.statusCode == 404 {
-                print("Page not found. Check url request string.")
-            }
-        }
-    }.resume()
-}
-
 // MARK: - Use case
+
+let client = HTTPClient()
 
 // MARK: - Get currency data
 
+let cbrApi = CBRApi(client: client)
 let currentDate = Date.getCurrentDate()
-let urlRequest = "http://\(Constants.cbrHost)/scripts/XML_daily.asp?date_req=\(currentDate)"
-let urlRequest404 = "http://\(Constants.cbrHost)/sc/XML_daily.asp?date_req=\(currentDate)"
-let urlRequestHostNotFound = "http://www.cb.ru/scripts/XML_daily.asp?date_req=\(currentDate)"
-let urlRequestBadParameter = "http://\(Constants.cbrHost)/scripts/XML_daily.asp?date_req=01/01/1970"
+cbrApi.getCurrencyExchangeList(dateString: currentDate)
+cbrApi.getFakeResource()
+cbrApi.getHostNotFound()
+cbrApi.getCurrencyExchangeListWithBadParameter()
 
 getData(urlRequest: urlRequest)
