edfd1ee754a74495e667b5d322d8ab1a7939f76f
ivanovdale <dania-spb@mail.ru>
2024-03-20 13:12:36 +0300

    implement error handling

--- Contents.swift
+++ Contents.swift
@@ -4,12 +4,20 @@ func getData(urlRequest: String) {
     let urlRequest = URL(string: urlRequest)
     guard let url = urlRequest else { return }
     URLSession.shared.dataTask(with: url) { data, response, error in
-        if error != nil {
-
-        } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
             guard let data = data else { return }
             let dataAsString = String(data: data, encoding: .utf8)
             
+        if let error = error as? URLError {
+            print("Error occured.")
+            switch error.code {
+            case .notConnectedToInternet:
+                print("The Internet connection appears to be offline. Please reconnect")
+            case .cannotFindHost:
+                let host = url.host ?? "Unknown"
+                print("Cannot find host \(host)")
+            default: print("An error occurred: \(error.localizedDescription)")
+            }
+        } else if let response = response as? HTTPURLResponse {
         }
     }.resume()
 }
